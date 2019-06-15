7af29771b533e68b3bcbeff20b8bbba8e21ac2a6
Alex Culligan <aculligan@icloud.com>
2019-06-15 00:00:59 -0500

    ga

--- _es6/ga.js
+++ _es6/ga.js
@@ -1,4 +1,4 @@
-/*(function () {
+(function () {
   let windowURL = window.location.href;
   let rootURL = '{{ site.url }}';
   let urlSplit = windowURL.split('/');
@@ -16,13 +16,4 @@
     page: location.pathname,
     title: thisPage
   });
-})();*/
-
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', '{{ site.ga }}', 'auto');
-ga('send', 'pageview');
+})();
