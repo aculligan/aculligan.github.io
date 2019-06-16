37978a94ab1be46a36d338d2621b6fab9b87fd16
Alex Culligan <aculligan@icloud.com>
2019-06-15 23:14:49 -0500

    GA update

--- CDN/Universal_App_Assets/ga_message.js
+++ CDN/Universal_App_Assets/ga_message.js
@@ -7,8 +7,9 @@
 		}
 		else 
 		{
-			throw new Error('You are currently using an ad blocker. Ad blockers will cause things not to work properly in some cases. Please whitelist this website and reload the page. Be happy, there are no ads here! :)');
+			document.body.innerHTML = '';
 			alert('You are currently using an ad blocker. Ad blockers will cause things not to work properly in some cases. Please whitelist this website and reload the page. Be happy, there are no ads here! :)');
+			throw new Error('You are currently using an ad blocker. Ad blockers will cause things not to work properly in some cases. Please whitelist this website and reload the page. Be happy, there are no ads here! :)');
 		}
 	});
 })();
