fd75df3a7b48cc61286e7aafcd3a3bde1ccac7f7
Alex Culligan <aculligan@icloud.com>
2019-06-16 01:13:50 -0500

    Update ga_message.js

--- CDN/Universal_App_Assets/ga_message.js
+++ CDN/Universal_App_Assets/ga_message.js
@@ -13,10 +13,10 @@
 			});
 			setTimeout(function () {
 				var leftDiv = $(".left-div");
-				leftDiv.text("You are currently using an ad blocker. Ad blockers will cause this website not to work properly in some cases. Please whitelist this website and reload the page. Don't worry, there are no ads here! :)").css({"color": "red","font-size": "200%", "font-weight": "bold", "text-align":"center","background-color":"#FFCC00", "padding-top": "200px", "width": "100%", "z-index": "10000000"});
-				//alert("You are currently using an ad blocker. Ad blockers will cause this website not to work properly in some cases. Please whitelist this website and reload the page. Don't worry, there are no ads here! :)");
+				leftDiv.text("You are currently using an ad blocker. Ad blockers will cause the site not to work properly. Please whitelist this domain and reload the page. Don't worry, there are no ads here! :)").css({"color": "red","font-size": "200%", "font-weight": "bold", "text-align":"center","background-color":"#FFCC00", "padding-top": "200px", "width": "100%", "z-index": "10000000"});
+				//alert("You are currently using an ad blocker. Ad blockers will cause the site not to work properly. Please whitelist this domain and reload the page. Don't worry, there are no ads here! :)");
 			}, 15000);
-			throw new Error("You are currently using an ad blocker. Ad blockers will cause things not to work properly in some cases. Please whitelist this website and reload the page. Don't worry, there are no ads here! :)");
+			throw new Error("You are currently using an ad blocker. Ad blockers will cause the site not to work properly. Please whitelist this domain and reload the page. Don't worry, there are no ads here! :)");
 		}
 	});
 })();
