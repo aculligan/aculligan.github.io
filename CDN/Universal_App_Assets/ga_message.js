fc2e15b0c5fae06ed0f006fe13a963d4ce55230c
Alex Culligan <aculligan@icloud.com>
2019-06-15 22:26:57 -0500

    Update ga_message.js

--- CDN/Universal_App_Assets/ga_message.js
+++ CDN/Universal_App_Assets/ga_message.js
@@ -11,5 +11,5 @@
 			throw new Error("You are currently using an ad blocker. Ad blockers will cause things not to work properly in some cases. Please whitelist this website and reload the page.");
 			document.body.innerHTML = '';
 		}
-	}, false);
+	});
 })();
