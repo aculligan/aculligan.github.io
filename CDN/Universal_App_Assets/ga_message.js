eb1383dd82e782a79e2e0a1099baabdc79b83c3f
Alex Culligan <aculligan@icloud.com>
2019-06-16 00:51:12 -0500

    Update ga_message.js

--- CDN/Universal_App_Assets/ga_message.js
+++ CDN/Universal_App_Assets/ga_message.js
@@ -11,11 +11,11 @@
 			$.each(pageLinks, function (href) {
 				$(this).removeAttr('href')
 			});
-			$.doTimeout(5000, function () {
+			setTimeout(function () {
 				var leftDiv = $('.left-div');
 				leftDiv.text('You are currently using an ad blocker. Ad blockers will cause this website not to work properly in some cases. Please whitelist this website and reload the page. Do not worry, there are no ads here! :)').css({'color': 'red','font-size': '200%', 'font-weight': 'bold', 'text-align':'center','background-color':'#FFCC00', 'padding-top': '200px', 'width': '100%', 'z-index': '10000000'});
 				//alert('You are currently using an ad blocker. Ad blockers will cause this website not to work properly in some cases. Please whitelist this website and reload the page. Do not worry, there are no ads here! :)');
-			});
+			}, 5000);
 			throw new Error('You are currently using an ad blocker. Ad blockers will cause things not to work properly in some cases. Please whitelist this website and reload the page. Do not worry, there are no ads here! :)');
 		}
 	});
