2c0cbbaa5bf2fd096f54c2ce8a23ced637969054
Alex Culligan <aculligan@icloud.com>
2019-06-15 20:28:09 -0500

    Update ga-min.js

--- assets/js/ga-min.js
+++ assets/js/ga-min.js
@@ -1,5 +1,22 @@
 ---
 ---
 
-'use strict';
-(function(){var a=window.location.href,c=a.split('/'),d;d=a==='{{ site.url }}'?'index':_.last(c),ga('create','{{ site.ga }}','auto'),ga('send',{hitType:'pageview',page:location.pathname,title:d})})();
+(function () {
+  let windowURL = window.location.href;
+  let rootURL = '{{ site.url }}';
+  let urlSplit = windowURL.split('/');
+  let thisPage;
+
+  if (windowURL === rootURL) {
+    thisPage = 'index';
+  } else {
+    thisPage = _.last(urlSplit);
+  }
+
+  ga('create', '{{ site.ga }}', 'auto');
+  ga('send', {
+    hitType: 'pageview',
+    page: location.pathname,
+    title: thisPage
+  });
+})();
