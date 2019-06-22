eb61c42ff20f534dee7fca7060987a6fe8c5602f
Alex Culligan <aculligan@icloud.com>
2019-06-21 20:02:30 -0500

    Conditionals

--- CDN/Zendesk_Priority_Highlights/script-normal-min.js
+++ CDN/Zendesk_Priority_Highlights/script-normal-min.js
@@ -1,63 +1,67 @@
-console.log('applied');
-if (window.location.href.indexOf('agent/filters') > 0) {
-  console.log('loaded');
-  const removeNormalHighlight = function removeOldNormal () {
-    let $prevHighlightNormal = $('.zph-ntp');
-    $prevHighlightNormal.removeClass('zph-ntp zph-hltd');
-  };
+const removeNormalHighlight = function removeOldNormal () {
+  let $prevHighlightNormal = $('.zph-ntp');
+  $prevHighlightNormal.removeClass('zph-ntp zph-hltd');
+};
 
-  const applyNormalHighlight = function highlightNormal () {
-    if (window.location.href.indexOf('agent/filters') > 0) {
-      let $tableHeadRow = $('#main_panes > section.ember-view.main_panes.split_pane.flush_top.collapsible.filters > div.pane.right.section > div > div > div > div > div > table > thead > tr');
-      let $tableHeadRowChildren = $tableHeadRow.children();
-      let priorityItemIndex
+const applyNormalHighlight = function highlightNormal () {
+  let $tableHeadRow = $('#main_panes > section.ember-view.main_panes.split_pane.flush_top.collapsible.filters > div.pane.right.section > div > div > div > div > div > table > thead > tr');
+  let $tableHeadRowChildren = $tableHeadRow.children();
+  let priorityItemIndex
 
-      $tableHeadRowChildren.each(function (index) {
-        let $thisRowChild = $(this);
-        let childHeaderName = $thisRowChild.text();
-        if (childHeaderName == 'Priority') {
-          priorityItemIndex = index;
-          //console.log(priorityItemIndex);
-        }
-      });
-
-      let $ticketRows = $('#main_panes > section > div.pane.right.section > div > div > div > div > div > div > table > tbody > tr');
-      let fieldFound
-
-      $ticketRows.each(function () {
-        let $thisRow = $(this);
-        let $thisRowChildren = $thisRow.children();
-        let $priorityCell = $thisRowChildren.eq(priorityItemIndex);
-        let $priorityCellText = $priorityCell.text();
-        fieldFound = $priorityCellText;
-        //console.log($priorityCellText);
-
-        if ($priorityCellText == 'Normal') {
-          $(this).addClass('zph-ntp zph-hltd');
-        }
-      });
-
-      //console.log(fieldFound);
-      if (fieldFound !== 'Low' && fieldFound !== 'Normal' && fieldFound !== 'High' && fieldFound !== 'Urgent') {
-        console.error('Zendesk Priority Highlights: Could not find Priority Field');
-      }
+  $tableHeadRowChildren.each(function (index) {
+    let $thisRowChild = $(this);
+    let childHeaderName = $thisRowChild.text();
+    if (childHeaderName == 'Priority') {
+      priorityItemIndex = index;
+      //console.log(priorityItemIndex);
     }
-  };
+  });
 
-  $(document).ready(function () {
+  let $ticketRows = $('#main_panes > section > div.pane.right.section > div > div > div > div > div > div > table > tbody > tr');
+  let fieldFound
+
+  $ticketRows.each(function () {
+    let $thisRow = $(this);
+    let $thisRowChildren = $thisRow.children();
+    let $priorityCell = $thisRowChildren.eq(priorityItemIndex);
+    let $priorityCellText = $priorityCell.text();
+    fieldFound = $priorityCellText;
+    //console.log($priorityCellText);
+
+    if ($priorityCellText == 'Normal') {
+      $(this).addClass('zph-ntp zph-hltd');
+    }
+  });
+
+  //console.log(fieldFound);
+  if (fieldFound !== 'Low' && fieldFound !== 'Normal' && fieldFound !== 'High' && fieldFound !== 'Urgent') {
+    console.error('Zendesk Priority Highlights: Could not find Priority Field');
+  }
+};
+
+$(document).ready(function () {
+  if (window.location.href.indexOf('agent/filters') > 0) {
     removeNormalHighlight ();
     setTimeout(applyNormalHighlight, 1700);
-  });
+  } else {
+    console.log('Zendesk Priority Highlights will only run on the "Views" page');
+  }
+});
 
-  $('*').click(function () {
+$('*').click(function () {
+  if (window.location.href.indexOf('agent/filters') > 0) {
     removeNormalHighlight ();
     setTimeout(applyNormalHighlight, 1300);
-  });
+  } else {
+    console.log('Zendesk Priority Highlights will only run on the "Views" page');
+  }
+});
 
-  $(window).focus(function () {
+$(window).focus(function () {
+  if (window.location.href.indexOf('agent/filters') > 0) {
     removeNormalHighlight ();
     setTimeout(applyNormalHighlight, 1300);
-  });
-} else {
-  console.log('Zendesk Priority Highlights will only run on the "Views" page');
-}
+  } else {
+    console.log('Zendesk Priority Highlights will only run on the "Views" page');
+  }
+});
