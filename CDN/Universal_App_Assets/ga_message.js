e40472b423623757450c05b1abc8bae4a9ed3af7
Alex Culligan <aculligan@icloud.com>
2019-06-22 22:14:20 -0500

    Garden.css

--- CDN/Universal_App_Assets/ga_message.js
+++ CDN/Universal_App_Assets/ga_message.js
@@ -1,11 +1,11 @@
 (function () {
 	window.addEventListener("load", function()
 	{
-		if(window.ga && ga.create) 
+		if(window.ga && ga.create)
 		{
 			console.log("Everything should work properly.");
 		}
-		else 
+		else
 		{
 			var pageLinks = $("a");
 			$.each(pageLinks, function (href) {
@@ -14,7 +14,6 @@
 			setTimeout(function () {
 				var leftDiv = $(".left-div");
 				leftDiv.text("You are currently using an ad blocker. Ad blockers will cause the site not to work properly. Please whitelist this domain and reload the page. Don't worry, there are no ads here! :)").css({"color": "red","font-size": "200%", "font-weight": "bold", "text-align":"center","background-color":"#FFCC00", "padding-top": "200px", "width": "100%", "z-index": "10000000"});
-				//alert("You are currently using an ad blocker. Ad blockers will cause the site not to work properly. Please whitelist this domain and reload the page. Don't worry, there are no ads here! :)");
 			}, 15000);
 			throw new Error("You are currently using an ad blocker. Ad blockers will cause the site not to work properly. Please whitelist this domain and reload the page. Don't worry, there are no ads here! :)");
 		}
