42abbc62de27ccfea4b23cd07d0fc46ec2d0e014
Alex Culligan <16241656+aculligan@users.noreply.github.com>
2019-07-06 19:52:49 -0500

    ga update

--- assets/js/ga-min.js
+++ assets/js/ga-min.js
@@ -3,6 +3,10 @@
 
 (function () {
 	window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+	{% if page.require contains 'uninstall' %}
+	ga('create', 'UA-87536814-1', 'auto');
+	{% else %}
 	ga('create', '{{ site.ga }}', 'auto');
+	{% endif %}
 	ga('send', 'pageview');
 })();
